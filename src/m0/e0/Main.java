72c533fbd18023e1af38e6f33781d62fc23504da
DDBull <dyussupov.d@gmail.com>
2021-05-16 18:11:23 +0500

    Tuple

---
+++ src/m0/e0/Main.java
@@ -0,0 +1,12 @@
+package m0.e0;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Tuple student1 = new Tuple(1, "Slava");
+        final String name = (String) student1.getRight();
+
+        System.out.println(name);
+    }
+
+}
